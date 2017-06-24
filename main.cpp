10f309e252ca4f25eeb50dd62c01dd2e207c636a
Dale Kim <dtkim1@gmail.com>
2017-06-23 20:38:14 -0700

    Enable Win32 surface extension and create a surface.  Probably isn't correct!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <cstdio>
 #include <cstdlib>
 #include <malloc.h>
+
+#define VK_USE_PLATFORM_WIN32_KHR
 #include <vulkan/vulkan.h>
+#include <Windows.h>
 
 void* VulkanAlignedAlloc(void* userdata, size_t bytes, size_t alignment, VkSystemAllocationScope alloc_scope)
 {
@@ -27,26 +30,19 @@ void VulkanInternalFreeNotify(void* userdata, size_t bytes, VkInternalAllocation
 }
 
 #define VK_CHECK(vk_result)  do { VkResult result = (vk_result); if (result != VK_SUCCESS) { printf("%s:%d got %d!\n", __FILE__, __LINE__, result); getchar(); return 1; } } while(0)
+#define ARRAY_COUNT(a)  (sizeof(a) / sizeof(a[0]))
 
-static uint32_t VulkanMajorVersion(uint32_t api_version)
+const char* const g_EnabledExtensions[] =
 {
-  return api_version >> 22;
-}
-
-static uint32_t VulkanMinorVersion(uint32_t api_version)
-{
-  return (api_version << 10) >> 22;
-}
-
-static uint32_t VulkanPatchVersion(uint32_t api_version)
-{
-  return (api_version << 22) >> 22;
-}
+  "VK_KHR_win32_surface",
+};
 
 int main()
 {
   VkInstanceCreateInfo info = {};
   info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  info.enabledExtensionCount = ARRAY_COUNT(g_EnabledExtensions);
+  info.ppEnabledExtensionNames = g_EnabledExtensions;
 
   VkAllocationCallbacks callbacks = {};
   callbacks.pfnAllocation = VulkanAlignedAlloc;
@@ -68,7 +64,7 @@ int main()
     VkPhysicalDeviceProperties properties = {};
     vkGetPhysicalDeviceProperties(physical_devices[i], &properties);
     printf("physical_devices[%u]:\n", i);
-    printf("  Vulkan API %u.%u.%u\n", VulkanMajorVersion(properties.apiVersion), VulkanMinorVersion(properties.apiVersion), VulkanPatchVersion(properties.apiVersion));
+    printf("  Vulkan API %u.%u.%u\n", VK_VERSION_MAJOR(properties.apiVersion), VK_VERSION_MINOR(properties.apiVersion), VK_VERSION_PATCH(properties.apiVersion));
     printf("  Vulkan driver version: %u\n", properties.driverVersion);
     printf("  Vendor: %u\n", properties.vendorID);
     printf("  Device: %u\n", properties.deviceID);
@@ -89,14 +85,28 @@ int main()
     printf("  Min image transfer granularity: (%u, %u, %u)\n", queue_properties[i].minImageTransferGranularity.width, queue_properties[i].minImageTransferGranularity.height, queue_properties[i].minImageTransferGranularity.depth);
   }
 
-  VkDevice device = {};
+  float queue_priorities[32] = {};
   VkDeviceQueueCreateInfo queue_create_info = {};
+  queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+  queue_create_info.queueFamilyIndex = 0;
+  queue_create_info.queueCount = queue_properties[0].queueCount;
+  queue_create_info.pQueuePriorities = queue_priorities;
+
   VkDeviceCreateInfo device_create_info = {};
   device_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
   device_create_info.queueCreateInfoCount = 1;
   device_create_info.pQueueCreateInfos = &queue_create_info;
+  VkDevice device = {};
   VK_CHECK(vkCreateDevice(physical_devices[0], &device_create_info, &callbacks, &device));
 
+  VkWin32SurfaceCreateInfoKHR surface_create_info = {};
+  surface_create_info.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
+  //surface_create_info.hinstance = ; what to do?
+  surface_create_info.hwnd = GetActiveWindow();
+  VkSurfaceKHR surface = {};
+  VK_CHECK(vkCreateWin32SurfaceKHR(instance, &surface_create_info, &callbacks, &surface));
+
+  vkDestroySurfaceKHR(instance, surface, &callbacks);
   vkDestroyDevice(device, &callbacks);
   vkDestroyInstance(instance, &callbacks);
   printf("Vulkan succeeded!\n");
